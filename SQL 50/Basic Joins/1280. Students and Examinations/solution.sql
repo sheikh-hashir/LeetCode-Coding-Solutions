49b20a609c5e272d28037507175c89eb7c0fd1e1
hashir_irfan <hashir.irfan@tkxel.com>
2024-08-09 15:20:15 +0500

    master | Solved Students and Examinations using CROSS JOIN and LEFT JOIN along with GROUP BY. Added README.md for full explanation

---
+++ SQL 50/Basic Joins/1280. Students and Examinations/solution.sql
@@ -0,0 +1,14 @@
+SELECT s.student_id,
+       s.student_name,
+       sub.subject_name,
+       count(e.subject_name) as attended_exams
+FROM Students s
+    CROSS JOIN Subjects sub
+    LEFT JOIN Examinations e
+        ON s.student_id = e.student_id
+           AND sub.subject_name = e.subject_name
+GROUP BY s.student_id,
+         s.student_name,
+         sub.subject_name
+ORDER BY s.student_id,
+         sub.subject_name;
