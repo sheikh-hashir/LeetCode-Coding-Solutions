55489be91fd010bbc72871f080a35721206d6262
sheikh-hashir <hashirirfan15@gmail.com>
2024-08-15 14:27:42 +0500

    master | Solved Confirmation Rate Using Left Join and Case. Added a useful testcase and README.md for explanation

---
+++ SQL 50/Basic Joins/1934. Confirmation Rate/solution.sql
@@ -0,0 +1,6 @@
+SELECT
+    s.user_id,
+    CASE
+        WHEN SUM(c.action = "confirmed") > 0 THEN ROUND(SUM(c.action = "confirmed")/COUNT(*), 2)
+    ELSE 0 END as confirmation_rate
+FROM Signups s LEFT JOIN Confirmations c ON s.user_id = c.user_id GROUP BY s.user_id
