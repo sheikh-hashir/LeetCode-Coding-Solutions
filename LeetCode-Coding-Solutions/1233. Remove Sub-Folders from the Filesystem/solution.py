e3ccf017011ada12653009d7a787cf7c8c035cfd
sheikh-hashir <hashirirfan15@gmail.com>
2024-11-04 15:18:50 +0500

    master | black applied on Remove Sub-Folders from the FileSystem

--- LeetCode-Coding-Solutions/1233. Remove Sub-Folders from the Filesystem/solution.py
+++ LeetCode-Coding-Solutions/1233. Remove Sub-Folders from the Filesystem/solution.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
         # Sort the folders to ensure that subfolders follow their parent folders.
@@ -7,7 +8,7 @@ class Solution:
         result = []
 
         for f in folder:
-            if not result or not f.startswith(f'{result[-1]}/'):
+            if not result or not f.startswith(f"{result[-1]}/"):
                 result.append(f)
 
         return result
