923abf19bc9e9184aed1da49d13f7cac28d52e4c
sheikh-hashir <hashirirfan15@gmail.com>
2024-11-04 15:35:07 +0500

    master | Solved Create a DataFrame from List using pd.DataFrame and columns parameter

---
+++ Pandas/Pandas Data Structures/2877. Create a DataFrame from List/solution.py
@@ -0,0 +1,7 @@
+from typing import List
+
+import pandas as pd
+
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    return pd.DataFrame(student_data, columns=["student_id", "age"])
