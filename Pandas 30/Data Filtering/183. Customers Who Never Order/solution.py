358fa312bf66358d2d9fa9b84caf62e8b629d688
sheikh-hashir <hashirirfan15@gmail.com>
2024-11-07 15:25:57 +0500

    master | Solved Customers Who Never Order Panda Problem using isin and rename columns

---
+++ Pandas 30/Data Filtering/183. Customers Who Never Order/solution.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    return customers[~(customers["id"].isin(orders["customerId"]))][["name"]].rename(
+        columns={"name": "Customers"}
+    )
